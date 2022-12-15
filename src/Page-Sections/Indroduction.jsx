d3c9df49a9327fdbd9a804c85d6469b6d8d3fda4
Dev Rook <110010478+Dev-Rook@users.noreply.github.com>
2022-12-15 18:06:20 -0500

    Upload

--- src/Page-Sections/Indroduction.jsx
+++ src/Page-Sections/Indroduction.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import Styles from "../Styles/Page-Section-Styles/Introduction.module.scss";
 
 const Indroduction = () => {
   return (
-    <div>Indroduction</div>
-  )
-}
+    <div className={Styles.Section}>
+      <div className={Styles.Content_Container}>
+        <div className={Styles.Images_Container}>
+          <div className={Styles.Image_Container_1}></div>
+          <div className={Styles.Image_Container_2}></div>
+        </div>
 
-export default Indroduction
+        <div className={Styles.About_Container}>
+          <p className={Styles.Question}>- Who We Are</p>
+          <p className={Styles.Brand}>Daily's Construction & Hardware</p>
+          <p className={Styles.Description}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+            cupiditate, <br /> repellendus quas optio porro labore, facere dolor nemo
+            non, totam saepe aliquam <br /> nulla accusamus velit animi placeat!
+            Maxime, necessitatibus dolorem.
+          </p>
+          <p className={Styles.Description}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+            cupiditate, <br /> repellendus quas optio porro labore, facere dolor nemo
+            non, totam saepe aliquam <br /> nulla accusamus velit animi placeat!
+            Maxime, necessitatibus dolorem.
+          </p>
+
+          <button className={Styles.Read_More_Button}>Read More</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Indroduction;
--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -3,12 +3,14 @@ import Styles from "../Styles/Page-Styles/Landing.module.scss"
 
 // Page Section Import Start
 import Hero from '../Page-Sections/Hero'
+import Introduction from "../Page-Sections/Indroduction"
 // Page Section Import End
 
 const Landing = () => {
   return (
     <div className={Styles.Page}>
       <Hero />
+      <Introduction />
     </div>
   )
 }
