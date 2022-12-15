e5da663fcea63b260702c4f20350b20304ef305d
Dev Rook <110010478+Dev-Rook@users.noreply.github.com>
2022-12-14 20:30:01 -0500

    Navbar start

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
   return (
     <div className={Styles.App}>
       <BrowserRouter>
+      <Navbar />
         <Routes>
           <Route path={"/"} element={<Landing />} />
           <Route path={"Contact"} element={<Contact />} />
--- src/Components/Main/Navbar.jsx
+++ src/Components/Main/Navbar.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import Styles from "../../Styles/Compenent-Styles/Navbar.module.scss";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className={Styles.Navbar}>
+      <div className={Styles.Brand_Container}>
+        <p className={Styles.Brand}>Daily's Contruction</p>
+      </div>
 
-export default Navbar
+      <ul className={Styles.Navlink_Container}>
+        <li className={Styles.NavItem}>
+          <Link className={Styles.Navlink} to={"/"}>
+            Home
+          </Link>
+        </li>
+        <li className={Styles.NavItem}>
+          <Link className={Styles.Navlink} to={"Services"}>
+            Services
+          </Link>
+        </li>
+        <li className={Styles.NavItem}>
+          <Link className={Styles.Navlink} to={"Projects"}>
+            Projects
+          </Link>
+        </li>
+        <li className={Styles.NavItem}>
+          <Link className={Styles.Navlink} to={"About"}>
+            About
+          </Link>
+        </li>
+        <li className={Styles.NavItem}>
+          <Link className={Styles.Navlink} to={"Articles"}>
+            Articles
+          </Link>
+        </li>
+        <li className={Styles.NavItem}>
+          <Link className={Styles.Navlink} to={"Contact"}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
