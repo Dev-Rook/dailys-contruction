8cce89b2513b475a9cff5ae5d659287625036814
Dev Rook <110010478+Dev-Rook@users.noreply.github.com>
2022-12-15 22:11:40 -0500

    Hero Slider

--- src/Page-Sections/Hero.jsx
+++ src/Page-Sections/Hero.jsx
@@ -1,21 +1,65 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import {motion} from "framer-motion"
-import Styles from "../Styles/Page-Section-Styles/Hero.module.scss"
+import React, {useState} from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import Styles from "../Styles/Page-Section-Styles/Hero.module.scss";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, FreeMode, Autoplay, Mousewheel } from "swiper";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+// import "swiper/css/effect-fade ";
+
+import HeroData from "../Assets/Data/Hero.json"
 
 const Hero = () => {
+
+  const [data, setData] = useState(HeroData)
+
   return (
     <div className={Styles.Section}>
-        <div className={Styles.Content_Container}>
-            <p className={Styles.Small_Text}>Architecture / Construction</p>
-            <p className={Styles.Large_Text}>
-                We Are Expert Builders
-            </p>
+      <div className={Styles.Content_Container}>
+        <p className={Styles.Small_Text}>Architecture / Construction</p>
+        <p className={Styles.Large_Text}>We Are Expert Builders</p>
 
-            <button className={Styles.Project_Button}>Our Projects</button>
-        </div>
+        <button className={Styles.Project_Button}>Our Projects</button>
+      </div>
+
+      <div className={Styles.Slider_Container}>
+        <Swiper
+          slidesPerView={1}
+          speed={1000}
+          spaceBetween={0}
+          modules={[Navigation, FreeMode, Mousewheel, Autoplay]}
+          autoplay={{
+            delay: 4000,
+            disableOnInteraction: false,
+          }}
+          grabCursor={true}
+          loop
+          pagination={{
+            dynamicBullets: true,
+          }}
+          mousewheel={true}
+          className={Styles.Swiper}
+        >
+          {data &&
+            data.map((value) => {
+              return <SwiperSlide key={value?.id}>
+                <div className={Styles.Image_Container}>
+                  <img src={value.Image} alt="" className={Styles.Image} />
+                </div>
+              </SwiperSlide>;
+            })}
+        </Swiper>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
